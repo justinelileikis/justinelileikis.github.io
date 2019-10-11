73830ed55eb5f8efc0b2c54104da86314d0ea7c7
Justine Lileikis <justine@res.im>
2019-10-11 17:51:10 -0400

    Initial commit - project set up

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './Header'
+import Main from './Main';
+
+const App = () => (
+	<div>
+		<div>
+			<Header />
+		</div>
+		<div>
+			<Main />
+		</div>
+	</div>
+)
+
+export default App
---
+++ src/components/Main.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+import Home from './Home'
+import Projects from './Projects'
+import Single from './Single'
+
+const Main = () => (
+  <main>
+    <Switch>
+		<Route exact path='/' component={Home}/>
+		<Route path='/projects' component={Projects}/>
+		<Route path='/projects/:url' component={Single}/>
+    </Switch>
+  </main>
+)
+
+export default Main
